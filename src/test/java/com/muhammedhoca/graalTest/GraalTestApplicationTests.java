adc1159886b4565c10c0675d13b6969aba9ebf78
Muhammet Ali KAYA <info@beemeapp.com>
2023-09-17 15:04:55 +0300

    Youtube için graalVm ilk örnek kodlamaları

---
+++ src/test/java/com/muhammedhoca/graalTest/GraalTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muhammedhoca.graalTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraalTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
